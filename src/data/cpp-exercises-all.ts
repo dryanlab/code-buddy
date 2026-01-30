b1ca8a10b7235b67e1fe1be56ed8315ed60eb253
Su Yan <suyan@YAN-Mac-Studio.local>
2026-01-30 10:58:17 -0500

    feat: C++ exercises-all aggregator (202 exercises)

---
+++ src/data/cpp-exercises-all.ts
@@ -0,0 +1,12 @@
+import { CPP_EXERCISES, type CppExercise } from './cpp-exercises';
+import { CPP_EXERCISES_PART2 } from './cpp-exercises-part2';
+import { CPP_EXERCISES_PART3 } from './cpp-exercises-part3';
+import { CPP_EXERCISES_PART4 } from './cpp-exercises-part4';
+
+export type { CppExercise };
+export const ALL_CPP_EXERCISES: CppExercise[] = [
+  ...CPP_EXERCISES,
+  ...CPP_EXERCISES_PART2,
+  ...CPP_EXERCISES_PART3,
+  ...CPP_EXERCISES_PART4,
+];
